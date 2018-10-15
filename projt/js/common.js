db7e8fbdc3551fc03f77f67ef0bbefccec37d915
Emil Sadykov <ferjx@mail.ru>
2018-10-16 03:12:00 +0500

    calls-online__16.10-3:00

--- projt/js/common.js
+++ projt/js/common.js
@@ -2,75 +2,79 @@ $(document).ready(function() {
 
 
 	$(document).ready(function(){
-  $('.slide').slick({
-  	infinite: true,
-  	dots: false,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 882,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        
-      }
-    },
-    
-   
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-   
+    $('.slide').slick({
+    	infinite: true,
+    	dots: false,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 882,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          
+        }
+      },
+      
+     
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+     
+    });
   });
-});
 
 
 
-$(window).load(function() {
+  $(window).load(function() {
 
-	$(".loader_inner").fadeOut();
-	$(".loader").delay(400).fadeOut("slow");
+  	$(".loader_inner").fadeOut();
+  	$(".loader").delay(400).fadeOut("slow");
 
-});
+  });
 
-});
 
-// accordion: faq
-$(function(){
-  const $uiAccordion = $('.js-ui-accordion');
+  // accordion: faq
+  $(function(){
+    const $uiAccordion = $('.js-ui-accordion');
 
-  $uiAccordion.accordion({
-    collapsible: true,
-    heightStyle: 'content',
+    $uiAccordion.accordion({
+      collapsible: true,
+      heightStyle: 'content',
 
-    activate: (event, ui) => {
-      const newHeaderId = ui.newHeader.attr('id');
+      activate: (event, ui) => {
+        const newHeaderId = ui.newHeader.attr('id');
 
-      if (newHeaderId) {
-        history.pushState(null, null, `#${newHeaderId}`);
+        if (newHeaderId) {
+          history.pushState(null, null, `#${newHeaderId}`);
+        }
+      },
+
+      create: (event, ui) => {
+        const $this = $(event.target);
+        const $activeAccordion = $(window.location.hash);
+
+        if ($this.find($activeAccordion).length) {
+          $this.accordion('option', 'active', $this.find($this.accordion('option', 'header')).index($activeAccordion));
+        }
       }
-    },
+    });
 
-    create: (event, ui) => {
-      const $this = $(event.target);
+    $(window).on('hashchange', event => {
       const $activeAccordion = $(window.location.hash);
+      const $parentAccordion = $activeAccordion.parents('.js-ui-accordion')
 
-      if ($this.find($activeAccordion).length) {
-        $this.accordion('option', 'active', $this.find($this.accordion('option', 'header')).index($activeAccordion));
+      if ($activeAccordion.length) {
+        $parentAccordion.accordion('option', 'active', $parentAccordion.find($uiAccordion.accordion('option', 'header')).index($activeAccordion));
       }
-    }
+    });
+
   });
 
-  $(window).on('hashchange', event => {
-    const $activeAccordion = $(window.location.hash);
-    const $parentAccordion = $activeAccordion.parents('.js-ui-accordion')
 
-    if ($activeAccordion.length) {
-      $parentAccordion.accordion('option', 'active', $parentAccordion.find($uiAccordion.accordion('option', 'header')).index($activeAccordion));
-    }
-  });
 
-})
+});
+
