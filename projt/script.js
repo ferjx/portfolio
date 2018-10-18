7a41a02d8257837e18319da2ab32ccb226b2dca6
Emil Sadykov <ferjx@mail.ru>
2018-10-19 03:03:48 +0500

    calls-online__17.10-3:00

--- projt/script.js
+++ projt/script.js
@@ -146,3 +146,67 @@ $(function() {
 	}	 
   });
 });
+
+// <ol> <li> list-style number vertical
+// <ol class="discharge-preview cols" rel="2">
+// As soon as the document structure has been loaded:
+document.addEventListener( "DOMContentLoaded", function() {
+    // For each html elem:
+    elems = document.getElementsByTagName("*"); // OL and UL wanted: chose all (*) here and select later.
+    for ( var e = 0; e < elems.length; e++ ) {
+        // Check if elem is a list (ordered/unordered) and has class name "cols":
+        if ( ( elems[e].tagName == "OL" || elems[e].tagName == "UL" ) && elems[e].className.search("cols") > -1 ) {
+            // Collect list items and number of columns (from the rel attribute):
+            var list = elems[e];
+            var listItems = list.getElementsByTagName("LI");
+            var Ncols = list.getAttribute("rel")*1; // *1 converts rel from string to int.
+            // Determine total number of items, items per column and column width:
+            var Ntotal = listItems.length;
+            var Npercol = Math.ceil( Ntotal/Ncols );
+            var colWidth = Math.floor( 100/Ncols )+"%";
+            // For each column:
+            for ( var c = 0; c < Ncols; c++ ) {
+                // Create column div:
+                var colDiv = document.createElement("DIV");
+                colDiv.style.cssFloat = "left";
+                colDiv.style.width = colWidth;
+                // Add list items to column div:
+                var i_start = c*Npercol;
+                var i_end = Math.min( (c+1)*Npercol, Ntotal );
+                for ( var i = i_start; i < i_end; i++ )
+                    colDiv.appendChild( listItems[0] ); // Using listItems[0] instead of listItems[i], because items are moved to colDiv!
+                // Add column div to list:  
+                list.appendChild( colDiv );
+            }
+        }
+    }
+} );
+
+
+$(function(){
+  // функция для мобильной версии
+  var setState = function() {
+    // условия для ширины и высоты окна
+    // if(length > yPos && width > xPos)
+
+    // условия события для окна
+    if( width > xPos) {
+      $( '#discharge-preview > div' ).css( {
+        width: "50%"
+      } );
+    } else {
+      $( '#discharge-preview > div' ).css( {
+        width: "100%"
+      } );
+    }
+  }
+
+  var xPos = 900;
+
+  var width = $(window).width();
+
+  $( window ).resize(function() {
+    width = $(window).width();
+    setState();
+  })
+});
